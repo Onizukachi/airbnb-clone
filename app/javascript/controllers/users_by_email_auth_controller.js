093e6285e298bcc00a0d7247fef914e0edfb657a
Onizukachi <dark_sao@mail.ru>
2024-02-15 11:02:00 +0300

    empty email state form message

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -1,15 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
+import axios from 'axios';
 
 export default class extends Controller {
-    static targets = [ "email", "submit" ];
+    static targets = [ "email", "submit", "emailWrapper", "invalidSvg", "errorMessage" ];
     connect() {
         this.submitTarget.addEventListener('click', (e) => {
             e.preventDefault();
 
             if (this.emailTarget.value.length === 0) {
-                // email field is empty, so don't do anything
+                this.emailWrapperTarget.classList.remove('focus-within:ring-1');
+                this.emailWrapperTarget.classList.remove('focus-within:ring-black');
+                this.emailWrapperTarget.classList.remove('focus-within:border-black');
+                this.emailWrapperTarget.classList.add('invalid-inset-input-text-field');
+                this.invalidSvgTarget.classList.remove('hidden');
+                this.errorMessageTarget.classList.remove('hidden');
             } else {
-                // do something
+                axios.get('api/users_by_email', {
+                    params: { email: this.emailTarget.value },
+                    headers: {
+                        'ACCEPT': 'application/json'
+                    }
+                }).then((response) => {
+                    Turbo.visit('users/sign_in');
+                }).catch((response) => {
+                    Turbo.visit('users/sign_up');
+                })
             }
         })
     }
